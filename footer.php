cf702c696af58adbefc402d39d0e7e9a41e4d423
itsjoshianurag <itsjoshianurag@gmail.com>
2022-05-07 23:39:38 +0530

    major changes

--- footer.php
+++
@@ -1,6 +0,0 @@
-<!-- footer.php -->
-<footer>
-        <div class="footer-love">
-            @2022, Anurag Joshi. Made with <i class="fa fa-heart pulse"></i> in India.
-        </div>
-</footer>
--- scripts.php
+++
@@ -1,6 +0,0 @@
-<!-- START scripts.php -->
-<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-<script type="text/javascript" src="js/jquery.easing.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
-<!-- END scripts.php -->
