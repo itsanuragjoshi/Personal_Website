dacaa241e517f8a27907b40cb043acb1fab69309
itsjoshianurag <itsjoshianurag@gmail.com>
2022-05-07 12:47:35 +0530

    major changes

---
+++ scripts.php
@@ -0,0 +1,6 @@
+<!-- START scripts.php -->
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<!-- END scripts.php -->
