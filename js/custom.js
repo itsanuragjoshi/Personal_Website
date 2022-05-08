42325ee83244c89cc22187a3b86582b0061dafd5
itsjoshianurag <itsjoshianurag@gmail.com>
2022-05-08 12:07:03 +0530

    final

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,23 @@
 //Tooltip
-$(document).ready(function(){
+$(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
 //Page Scroll Indicator
-$(document).ready(function() {
-  $(window).scroll(function() {
-    var wintop = $(window).scrollTop(); 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var wintop = $(window).scrollTop();
     var docheight = $(document).height();
     var winheight = $(window).height();
-    var scrolled = (wintop/(docheight-winheight))*100;
+    var scrolled = (wintop / (docheight - winheight)) * 100;
     $('.progress-bar').css('width', (scrolled + '%'));
   });
 });
 
-//Hamburger Menu
-$(document).ready(function() {
-  $('.icon-ham').click(function() {
+// Hamburger Menu
+$(document).ready(function () {
+  $('.icon-ham').click(function () {
     $('nav').fadeToggle(300);
-    $('.icon-ham').toggleClass('active'); 
+    $('.icon-ham').toggleClass('active');
   });
 });
